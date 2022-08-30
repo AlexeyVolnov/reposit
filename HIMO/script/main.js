ee58915aa2fe75101abb31faae8484ce4251da4d
Alexey <lexa1313.lexa@gmail.com>
2022-08-30 20:16:35 +0500

    1

--- HIMO/script/main.js
+++ HIMO/script/main.js
@@ -2,7 +2,31 @@ $(function () {
     $('.bike-slider').slick({
 
         arrows:false,
-        dots: true
+        dots: true,
+        autoplay:true,
+        autoplaySpeed:2000,
+        fade: true
+    });
+})
 
+$(function () {
+    $('.gallery').slick({
+
+        arrows:false,
+        dots: true,
+        autoplay:true,
+        autoplaySpeed:2000,
+        fade: true
+    });
+})
+
+$(function () {
+    $('.order-images').slick({
+
+        arrows:false,
+        dots: true,
+        autoplay:true,
+        autoplaySpeed:2000,
+        fade: true
     });
 })
