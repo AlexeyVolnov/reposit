6510fa32e9361740c15b40330444e4f4b4cffd5d
Alexey <lexa1313.lexa@gmail.com>
2022-09-05 17:23:49 +0500

    Revert "111"
    This reverts commit 3aa25992ebf9e71e3185e78bfb126ef47eb13dde, reversing
    changes made to 7d5290d72fd3fba0ad20d0768574557b2d082410.

---
+++ HIMO/script/main.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('.bike-slider, .gallery, .order-images ').slick({
+
+        arrows:false,
+        dots: true,
+        autoplay:true,
+        autoplaySpeed:2000,
+        fade: true
+    });
+})
