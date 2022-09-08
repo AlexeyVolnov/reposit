129ae00276ebcb218bbcce63ee717cc3bbfe00e6
Alexey <lexa1313.lexa@gmail.com>
2022-09-08 15:04:56 +0500

    tesla
    tesla

---
+++ tesla/js/main.js
@@ -0,0 +1,16 @@
+$(function () {
+    $('.present-slider').slick({
+        arrows:false,
+        autoplay:true,
+        autoplaySpeed:2000,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+
+
+
+
+        cssEase: 'linear'
+    });
+})
