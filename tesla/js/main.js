b387c3620b90cfcdc04070954c73cfbf6ac2f5d0
Alexey <lexa1313.lexa@gmail.com>
2022-09-16 14:47:40 +0500

    12
    12

--- tesla/js/main.js
+++ tesla/js/main.js
@@ -1,16 +1,21 @@
 $(function () {
     $('.present-slider').slick({
-        arrows:false,
-        autoplay:false,
-        autoplaySpeed:2000,
+        arrows: false,
+        autoplay: false,
+        autoplaySpeed: 2000,
         dots: true,
         infinite: true,
         speed: 300,
         fade: true,
-
-
-
-
         cssEase: 'linear'
     });
+
+    $('.product-menu').on('click',function(){
+        $('.present_nav').addClass('active');
+    })
+
+    $('.nav-close_btn').on('click',function(){
+        $('.present_nav').removeClass('active');
+    })
 })
+
