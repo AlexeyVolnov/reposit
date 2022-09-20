a953857a6e4cc363fdb2ebc0b7c589ccb078fd2a
Alexey <lexa1313.lexa@gmail.com>
2022-09-20 16:55:13 +0500

    1
    1

---
+++ MartaUP/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.header-slider').slick({
+        dots: true,
+        autoplay:false,
+        autoplaySpeed: 2000,
+        arrows: false,
+        fade: true,
+});
+});
