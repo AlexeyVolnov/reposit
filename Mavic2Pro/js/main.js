3a53a6bc9f3f36d630eba7d0e5104e6cf95ff422
Alexey <lexa1313.lexa@gmail.com>
2022-10-03 23:49:47 +0500

    2
    2

---
+++ Mavic2Pro/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function (){
+    $('.slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        arrows: true,
+    })
+
+
+})
