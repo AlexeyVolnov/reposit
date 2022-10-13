902edc55bbdadbf99d886cec6a223a1c0b7eeaeb
Alexey <lexa1313.lexa@gmail.com>
2022-10-13 15:43:49 +0500

    1
    1

--- mavic/gulpfile.js
+++ mavic/gulpfile.js
@@ -38,7 +38,7 @@ function styles() {
 
 }
 function images() {
-    return src('app/images/**/*')
+    return src('app/images/**/*.*')
         .pipe(imagemin([
             imagemin.gifsicle({interlaced: true}),
             imagemin.mozjpeg({quality: 75, progressive: true}),
