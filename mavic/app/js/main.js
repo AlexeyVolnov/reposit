894a0b7358ee04b52bd5f65f28f5fc979f1f843e
Alexey <lexa1313.lexa@gmail.com>
2022-10-18 16:26:01 +0500

    1
    1

--- mavic/app/js/main.js
+++ mavic/app/js/main.js
@@ -1,11 +1,12 @@
-$(document).ready(function (){
+$(document).ready(function () {
     $('.present-slider').slick({
-        dots: false,
-        infinite: true,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear',
-        arrows: true,
+        prevArrow: '<button class="slider-btn slider-btn-left"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
+            '<path d="M9.21839 1L1 9L9.21839 17" stroke="white"/>\n' +
+            '</svg></button>',
+        nextArrow: '<button class="slider-btn slider-btn-right"><svg width="10" height="18" viewBox="0 0 10 18" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
+            '<path d="M0.78161 17L9 9L0.78161 1" stroke="white"/>\n' +
+            '</svg></button>',
+        infinite: false,
     })
 
 
