793289a78b46b6eab6004e01174965380e1460b3
Alexey <lexa1313.lexa@gmail.com>
2022-10-25 15:09:45 +0500

    2
    2

--- burger/app/js/main.js
+++ burger/app/js/main.js
@@ -1,13 +1,32 @@
-function onEntry(entry) {
-   entry.forEach((change) => {
-      if (change.isIntersecting) {
-         change.target.classList.add("element-show");
-      }
-   });
+const hamb = document.querySelector("#hamb");
+const popup = document.querySelector("#popup");
+const body = document.body;
+
+
+const menu = document.querySelector("#menu").cloneNode(1);
+
+function hambHandler(e){
+    e.preventDefault();
+    popup.classList.toggle("open");
+    hamb.classList.toggle("active");
+    body.classList.toggle("noscroll");
+    renderPopup();
 }
-let options = { threshold: [0.5] };
-let observer = new IntersectionObserver(onEntry, options);
-let elements = document.querySelectorAll(".element-animation");
-for (let elm of elements) {
-   observer.observe(elm);
+function renderPopup(){
+    popup.appendChild(menu);
 }
+
+const links = Array.from(menu.children);
+links.forEach((link) => {
+    link.addEventListener('click',closeOnClick)
+});
+function closeOnClick (){
+    popup.classList.remove("open");
+    hamb.classList.remove("active");
+    body.classList.remove("noscroll");
+}
+
+hamb.addEventListener("click",hambHandler);
+
+
+
