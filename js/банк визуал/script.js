b8b7b05beb3031aaa84cc47bc5c2c9ff4a56fdf4
Alexey <57610468+AlexeyVolnov@users.noreply.github.com>
2023-01-19 16:24:37 +0500

    3
    3

--- js/банк визуал/script.js
+++ js/банк визуал/script.js
@@ -7,27 +7,44 @@ const modalWindow = document.querySelector('.modal-window');
 const overlay = document.querySelector('.overlay');
 const btnCloseModalWindow = document.querySelector('.btn--close-modal-window');
 const btnsOpenModalWindow = document.querySelectorAll(
-  '.btn--show-modal-window'
+    '.btn--show-modal-window'
 );
 
-const openModalWindow = function () {
-  modalWindow.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+const openModalWindow = function (e) {
+    e.preventDefault()
+    modalWindow.classList.remove('hidden');
+    overlay.classList.remove('hidden');
 };
 
 const closeModalWindow = function () {
-  modalWindow.classList.add('hidden');
-  overlay.classList.add('hidden');
+    modalWindow.classList.add('hidden');
+    overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModalWindow.length; i++)
-  btnsOpenModalWindow[i].addEventListener('click', openModalWindow);
+btnsOpenModalWindow.forEach((button) => {
+    button.addEventListener('click', openModalWindow);
+})
 
 btnCloseModalWindow.addEventListener('click', closeModalWindow);
 overlay.addEventListener('click', closeModalWindow);
 
 document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modalWindow.classList.contains('hidden')) {
-    closeModalWindow();
-  }
+    if (e.key === 'Escape' && !modalWindow.classList.contains('hidden')) {
+        closeModalWindow();
+    }
 });
+
+
+///////////
+
+
+const message = document.createElement('div');
+message.classList.add('cookie-message')
+message.innerHTML = `Мы используем на этом сайте cookie для улучшения функциональности. <button class='btn btn--close-cookie'>ОК!</button>`
+
+document.querySelector('.header').before(message)
+message.querySelector('.btn--close-cookie').addEventListener('click', function () {
+        message.remove()
+    })
+message.style.height = Number.parseFloat(getComputedStyle(message).height) + 50 + 'px';
+document.documentElement.style.setProperty('--color-first', 'rgba(79,40,218,0.46)')
