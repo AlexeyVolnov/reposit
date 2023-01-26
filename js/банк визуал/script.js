8f875edfb818c26c08097a1755dafb28b6d0272c
Alexey <57610468+AlexeyVolnov@users.noreply.github.com>
2023-01-26 12:57:21 +0500

    1
    3

--- js/банк визуал/script.js
+++ js/банк визуал/script.js
@@ -3,9 +3,19 @@
 ///////////////////////////////////////
 // Modal window
 
+
 const modalWindow = document.querySelector('.modal-window');
 const overlay = document.querySelector('.overlay');
 const btnCloseModalWindow = document.querySelector('.btn--close-modal-window');
+const nav = document.querySelector('.nav');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+const tabContainer = document.querySelector('.operations__tab-container');
+const contentBlock = document.querySelectorAll('.operations__content')
+const buttons = document.querySelectorAll('.operations__tab');
+const link = document.querySelector('.nav__links')
+
+
 const btnsOpenModalWindow = document.querySelectorAll(
     '.btn--show-modal-window'
 );
@@ -49,8 +59,7 @@ message.querySelector('.btn--close-cookie').addEventListener('click', function (
 message.style.height = Number.parseFloat(getComputedStyle(message).height) + 50 + 'px';
 document.documentElement.style.setProperty('--color-first', 'rgba(79,40,218,0.46)')
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
+
 // плавный скрол до элемента
 /*// старый метод скрола к элементу
 btnScrollTo.addEventListener('click', function (evt) {
@@ -67,12 +76,13 @@ btnScrollTo.addEventListener('click', function (evt) {
 
 
 //////скрол
-document.querySelector('.nav__links').addEventListener('click', function (e) {
+link.addEventListener('click', function (e) {
     e.preventDefault();
     if (e.target.classList.contains('nav__link') && !e.target.classList.contains('btn')) {
         const href = e.target.getAttribute('href');
         document.querySelector(`${href}`).scrollIntoView({behavior: 'smooth'})
     }
+    ;
 })
 // переключение вкладок
 //первый вариант
@@ -102,15 +112,13 @@ container.addEventListener('click',function (e){
     console.log(content)
 })
 */
-
-const tabContainer = document.querySelector('.operations__tab-container');
-const contentBlock = document.querySelectorAll('.operations__content')
-const buttons = document.querySelectorAll('.operations__tab');
-
+//// вкладки
 tabContainer.addEventListener('click', function (e) {
     const clickedButton = e.target.closest('.operations__tab');
-    if (!clickedButton) {return}
-    buttons.forEach(btn=>btn.classList.remove('operations__tab--active'))
+    if (!clickedButton) {
+        return
+    }
+    buttons.forEach(btn => btn.classList.remove('operations__tab--active'))
     contentBlock.forEach(block => block.classList.remove('operations__content--active'));
 
     const numberBtn = +clickedButton.getAttribute('data-tab')
@@ -118,3 +126,24 @@ tabContainer.addEventListener('click', function (e) {
     document.querySelector(`.operations__content--${numberBtn}`).classList.add('operations__content--active')
     document.querySelector(`.operations__tab--${numberBtn}`).classList.add('operations__tab--active')
 })
+////
+const navLinkHoverAnimation = function (e) {
+    const elemOver = e.target;
+    if (elemOver.classList.contains('nav__link')) {
+        const siblingLinks = elemOver.closest('.nav__links').querySelectorAll('.nav__link')
+        const logo = elemOver.closest('.nav').querySelector('.nav__logo');
+        const logoText = elemOver.closest('.nav').querySelector('.nav__text')
+        siblingLinks.forEach(link => {
+            if (link !== elemOver) {
+                link.style.opacity = this;
+                logoText.style.opacity = this;
+                logo.style.opacity = this
+            }
+        })
+    }
+}
+
+nav.addEventListener('mouseover', navLinkHoverAnimation.bind(0.4))
+
+nav.addEventListener('mouseout', navLinkHoverAnimation.bind(1))
+
