c7e0e46c40ebb9c90ef7cd96279f23f158048ed9
Alexey <57610468+AlexeyVolnov@users.noreply.github.com>
2023-03-27 02:15:52 +0500

    Add Promise
    1

--- js/countries/script.js
+++ js/countries/script.js
@@ -3,19 +3,15 @@
 const countries = document.querySelector('.countries');
 const menuCountries = document.querySelector('.menu-countries');
 
-let allCountries = [];
 
-
-const listCountries = new XMLHttpRequest();
-listCountries.open('GET', 'https://restcountries.com/v3.1/all');
-listCountries.send();
-listCountries.addEventListener('load', function() {
-  allCountries = JSON.parse(listCountries.responseText);
-  allCountries.forEach(countries => {
-    const html = `<li class='countries-name'>${countries.name.common}</li>`;
-    menuCountries.insertAdjacentHTML('beforeend', html);
-  });
-});
+const listCountries = fetch('https://restcountries.com/v3.1/all')
+listCountries
+    .then(response=>response.json()).then(data=>{
+    data.forEach(countries => {
+        const html = `<li class='countries-name'>${countries.name.common}</li>`;
+        menuCountries.insertAdjacentHTML('beforeend', html);
+    });
+})
 
 menuCountries.addEventListener('click', function(e) {
   if (e.target.closest('.countries-name')) {
@@ -26,6 +22,7 @@ menuCountries.addEventListener('click', function(e) {
   }
 
 });
+
 
 getCountryData('russia');
 
@@ -53,26 +50,19 @@ function displayCountries(data, className) {
 }
 
 function getCountryData(country) {
-  const request1 = new XMLHttpRequest();
-  request1.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-  request1.send();
-  request1.addEventListener('load', function() {
-    const [data1] = JSON.parse(this.responseText);
-    displayCountries(data1);
-
-    const fierstNeighbor = data1.borders;
-    if (!fierstNeighbor) return;
-    fierstNeighbor.forEach(item => {
-      const request2 = new XMLHttpRequest();
-      request2.open('GET', `https://restcountries.com/v3.1/alpha/${item}`);
-      request2.send();
-      request2.addEventListener('load', function() {
-        const [data2] = JSON.parse(this.responseText);
-        displayCountries(data2, 'neighbour');
-
-      });
-    });
-  });
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+      .then(response=>response.json()).then(data=>{
+          displayCountries(...data);
+          getNeighbor(...data)
+          });
 }
 
-
+function getNeighbor(data){
+    const fierstNeighbor = data.borders;
+    if (!fierstNeighbor) return;
+    fierstNeighbor.forEach(item => 
+        fetch(`https://restcountries.com/v3.1/alpha/${item}`)
+            .then(response=>response.json())
+            .then(data=>displayCountries(...data, 'neighbour'))
+    )
+}
